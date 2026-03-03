5ccde8b8dfae8064d5f004855c9ef085d13347c8
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-03 10:18:35 +0700

    Menambahkan code di interface class UserRepository di package repository

--- src/main/java/com/deploy/praktikum1/repository/UserRepository.java
+++ src/main/java/com/deploy/praktikum1/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.deploy.praktikum1.repository;
 
-public interface UserRepository {
+import com.deploy.praktikum1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
